e169f3d3518c68ea66d9e6cbb63d12b2fb10242b
mWasyluk <marekwasyluk@wp.pl>
2025-03-09 22:08:11 +0100

    ref: provide dynamic aliases

--- babel.config.js
+++ babel.config.js
@@ -6,19 +6,10 @@ module.exports = function (api) {
             [
                 'module-resolver',
                 {
-                    root: ['./'],
                     alias: {
-                        '@components': './src/components',
-                        '@constants': './src/constants',
-                        '@contexts': './src/contexts',
-                        '@models': './src/models',
-                        '@screens': './src/screens',
-                        '@services': './src/services',
-                        '@styles': './src/styles',
-                        '@utils': './src/utils',
+                        '@': './src',
                         '@assets': './assets',
                     },
-                    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
                 },
             ],
         ],
