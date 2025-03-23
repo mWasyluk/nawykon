fcc9e3046e52a6cb860715bc2aff94c54413ba2d
mWasyluk <marekwasyluk@wp.pl>
2025-03-23 16:38:29 +0100

    fix: font appears flattened on mobile

--- src/styles/fontStyles.js
+++ src/styles/fontStyles.js
@@ -4,38 +4,46 @@ const fontStyles = StyleSheet.create({
     header: {
         fontFamily: 'Genos_700Bold',
         fontSize: 20,
+        lineHeight: 24,
         textTransform: 'uppercase',
     },
     title: {
         fontFamily: 'Genos_700Bold',
         fontSize: 18,
+        lineHeight: 22,
         textTransform: 'uppercase',
     },
     body: {
         fontFamily: 'Genos_400Regular',
         fontSize: 16,
+        lineHeight: 20,
     },
     bodyBold: {
         fontFamily: 'Genos_700Bold',
         fontSize: 16,
+        lineHeight: 20,
     },
     caption: {
         fontFamily: 'Genos_400Regular',
         fontSize: 14,
+        lineHeight: 18,
     },
     label: {
         fontFamily: 'Genos_600SemiBold_Italic',
         fontSize: 14,
+        lineHeight: 18,
         textTransform: 'uppercase',
     },
     action: {
         fontFamily: 'Genos_600SemiBold',
         fontSize: 16,
+        lineHeight: 20,
         textTransform: 'uppercase',
     },
     actionLarge: {
         fontFamily: 'Genos_600SemiBold',
         fontSize: 20,
+        lineHeight: 24,
         textTransform: 'uppercase',
     },
 });
