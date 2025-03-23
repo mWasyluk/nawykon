9341cf411adf0aa286ed6d5663b56a3603a4ed54
mWasyluk <marekwasyluk@wp.pl>
2025-03-23 16:39:39 +0100

    feat: provide metrics for comps styling

--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -1,9 +1,10 @@
 const metrics = {
     headerHeight: 48,
+    titleHeight: 32,
     spacing: {
-        // xs: 5,
+        xs: 5,
         sm: 10,
-        // md: 20,
+        md: 20,
         // lg: 30,
         // xl: 40,
     },
@@ -13,12 +14,16 @@ const metrics = {
         // md: 20,
         // lg: 30,
         // xl: 40,
+        circular: 999,
     },
     imageSize: {
         xs: 15,
         sm: 20,
         md: 30,
         lg: 40,
+    },
+    buttonSize: {
+        sm: 32,
     }
 }
 
