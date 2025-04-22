65f8cc1d23350c3fe2c4def70996e8692e6f976e
mWasyluk <marekwasyluk@wp.pl>
2025-04-22 22:36:36 +0200

    fix: new activities does not show till date change

--- src/screens/commons/activity/ActivitySection.js
+++ src/screens/commons/activity/ActivitySection.js
@@ -54,7 +54,7 @@ export default function ActivitySection(props) {
 
     const dailyStatistics = useMemo(() => (
         monthlyStatistics.calendar[selectedDate]
-    ), [selectedDate]);
+    ), [selectedDate, activityRegistry]);
 
     const dailyMoodReport = useMemo(() => (
         activityRegistry.getRecord(selectedDate).mood
