9c6f34dbc2443cf9ef532830f4028c0e5dce7fc2
mWasyluk <marekwasyluk@wp.pl>
2025-04-22 17:04:17 +0200

    fix: auth bg color

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import ProvidersWrapper from '@contexts/ProvidersWrapper';
+import { colors } from '@styles';
 import { ExpoRoot } from 'expo-router';
 import { SafeAreaView } from 'react-native';
 import { RootSiblingParent } from 'react-native-root-siblings';
@@ -7,7 +8,7 @@ const ctx = require.context("./src/screens");
 
 export default function App() {
     return (
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: colors.light }}>
             <RootSiblingParent>
                 <ProvidersWrapper>
                     <ExpoRoot context={ctx} />
